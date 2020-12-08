69b84563068909384eeee8268b61046d3c9cb301
derockenthis <53790662+derockenthis@users.noreply.github.com>
2020-12-07 19:34:13 -0800

    Add files via upload

--- SL Ocean/app.py
+++ SL Ocean/app.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 boostrap = Bootstrap(app)
-dictionary = {"Derek":["6'5","19"]}
+
 @app.route('/')
 def home():
-    return render_template('index.html', my_dict = dictionary)
+    return render_template('index.html')
